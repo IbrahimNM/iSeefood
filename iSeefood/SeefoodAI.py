907777366a6225865fa0d6662fe005924dd11e21
Ibrahim <almohaimeed.6@wright.edu>
2019-03-27 10:47:09 -0400

    Modified the SeefoodAI class

--- iSeefood/SeefoodAI.py
+++ iSeefood/SeefoodAI.py
@@ -1,12 +1,12 @@
 '''
-    --------------- NOTE ------------------------------
+    --------------- NOTE -------------------------------
     THIS CLASS SHALL BE A SINGLETON. 
     CURRENTLY, WE ONLY NEED A SINGLE INSTANCE
     TO BE RUNNING. 
     ----------------------------------------------------
     [WHY?] AI OBJECT WILL CONSUME A LARGE AMOUNT OF MEMORY
            THUS A SINGLE INSTANCE WOULD BE ENOUGH FOR NOW.
-    -------------- CREDIT -------------------------------
+    -------------- CREDIT ------------------------------
     AI IS PROVIDED BY DR.DORAN FROM WRIGHT STATE
     UNIVERSITY.  
     -------------- AUTHOR ------------------------------
@@ -17,7 +17,7 @@
 '''
 
 class SeefoodAI(object):
-    # Single instance 
+    # Single private instance 
     __instance = None
 
     # Initilize an AI object to be running 
@@ -27,6 +27,7 @@ class SeefoodAI(object):
             raise Exception("This class is a Signleton!")
         else:
             SeefoodAI.__instance = self
+            SeefoodAI.__instance.__setup()
         print "Seefood AI instance has been created!"
         
     
@@ -36,4 +37,16 @@ class SeefoodAI(object):
         if SeefoodAI.__instance == None:
             SeefoodAI()
         return SeefoodAI.__instance
+    
+    
+    def __setup(self):
+        ''' Setting-up the SeefoodAI instance'''
+        print("Setting up instance ....")
+    
+    def submitImg(self, imagePath):
+        ''' Passing an image to the AI to be analyzed '''
+        print("Analyzing image.... Done!")
+        
+    
+    
     
--- iSeefood/main.py
+++ iSeefood/main.py
@@ -1,9 +1,11 @@
 from SeefoodAI import SeefoodAI
+
 def main():
     print 'Hello Ibrahim ... '
     # Get the singleton object of the SeefoodAI object.
     current = SeefoodAI.getInstance()
     print current
+    current.submitImg('ibrahim/')
     
 
 if __name__ == "__main__":
