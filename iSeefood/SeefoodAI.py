ae8d686fb1a79c2c1128336235f99fcc879ca554
Ibrahim <almohaimeed.6@wright.edu>
2019-04-01 14:44:35 -0400

    Add AI files, sample images, and modified SeefoodAI class.

--- iSeefood/SeefoodAI.py
+++ iSeefood/SeefoodAI.py
@@ -16,9 +16,23 @@
     ----------------------------------------------------
 '''
 
+import argparse
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
 class SeefoodAI(object):
     # Single private instance 
     __instance = None
+    
+    global sess, class_scores, x_input, keep_prob
+    sess = tf.Session()
+    saver = tf.train.import_meta_graph('saved_model/model_epoch5.ckpt.meta')
+    saver.restore(sess, tf.train.latest_checkpoint('saved_model/'))
+    graph = tf.get_default_graph()
+    x_input = graph.get_tensor_by_name('Input_xn/Placeholder:0')
+    keep_prob = graph.get_tensor_by_name('Placeholder:0')
+    class_scores = graph.get_tensor_by_name("fc8/fc8:0")
 
     # Initilize an AI object to be running 
     def __init__(self):
@@ -42,11 +56,22 @@ class SeefoodAI(object):
     def __setup(self):
         ''' Setting-up the SeefoodAI instance'''
         print("Setting up instance ....")
-    
-    def submitImg(self, imagePath):
-        ''' Passing an image to the AI to be analyzed '''
-        print("Analyzing image.... Done!")
         
     
-    
-    
+    def submitImg(self, image_path):
+        ''' Passing an image to the AI to be analyzed '''
+        image = Image.open(image_path).convert('RGB')
+        image = image.resize((227, 227), Image.BILINEAR)
+        img_tensor = [np.asarray(image, dtype=np.float32)]
+        print 'looking for food in ' + image_path
+
+        #Run the image in the model.
+        scores = sess.run(class_scores, {x_input: img_tensor, keep_prob: 1.})
+        print scores
+        # if np.argmax = 0; then the first class_score was higher, e.g., the model sees food.
+        # if np.argmax = 1; then the second class_score was higher, e.g., the model does not see food.
+        if np.argmax(scores) == 1:
+            print "No food here... :( "
+        else:
+            print "Oh yes... I see food! :D"
+        print("Analyzing image.... Done!")
--- iSeefood/main.py
+++ iSeefood/main.py
@@ -1,11 +1,22 @@
 from SeefoodAI import SeefoodAI
+import argparse
+
+parser = argparse.ArgumentParser(description="Ask SeeFood if there is food in the image provided.")
+parser.add_argument('image_path', help="The full path to an image file stored on disk.")
+
 
 def main():
     print 'Hello Ibrahim ... '
     # Get the singleton object of the SeefoodAI object.
+    
+    # Get the singleton object of the SeefoodAI object.
     current = SeefoodAI.getInstance()
     print current
-    current.submitImg('ibrahim/')
+
+    # Args handling 
+    args = parser.parse_args() 
+    image_path = args.image_path
+    current.submitImg(image_path)
     
 
 if __name__ == "__main__":
