58ad26a80b5a55a9bc535b07693628e7cd3c1dc8
Ibrahim <almohaimeed.6@wright.edu>
2019-04-06 14:11:50 +0300

    Allow user to input file path

--- iSeefood/main.py
+++ iSeefood/main.py
@@ -1,15 +1,11 @@
 from SeefoodAI import SeefoodAI
 import argparse
-
-parser = argparse.ArgumentParser(description="Ask SeeFood if there is food in the image provided.")
-parser.add_argument('image_path', help="The full path to an image file stored on disk.")
+import sys
 
 
 def main():
     print 'Hello friend, glad to see you here ... '
     
-    # Get the singleton object of the SeefoodAI object.
-    current = SeefoodAI.getInstance()
 
     # Args handling 
     '''
@@ -17,11 +13,22 @@ def main():
         - If there's no args passed, display message for user. 
         - If there're args passed, check their validity then continue
     '''
-
-    args = parser.parse_args() 
-    image_path = args.image_path
-    current.submitImg(image_path)
     
 
+    
+    # Get the singleton object of the SeefoodAI object.
+    current = SeefoodAI.getInstance()
+    try:
+        
+        print 'Enter image path: '
+        image_path = raw_input() # get file path from user
+        image_path = image_path.strip() # remove white spaces.
+        # TODO: Verify and validate file path
+        current.submitImg(image_path)
+           
+    except:
+        print '** Please check file path! **'    
+
+   
 if __name__ == "__main__":
    main()
