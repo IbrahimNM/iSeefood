492ecc6809c67d3e856cec8a5d6e90ae7117b904
Ibrahim <almohaimeed.6@wright.edu>
2019-04-07 14:07:48 +0300

    Remove old comments

--- iSeefood/main.py
+++ iSeefood/main.py
@@ -6,22 +6,11 @@ import sys
 def main():
     print 'Hello friend, glad to see you here ... '
     
-
-    # Args handling 
-    '''
-        - Check for args:
-        - If there's no args passed, display message for user. 
-        - If there're args passed, check their validity then continue
-    '''
-    
-
-    
     # Get the singleton object of the SeefoodAI object.
     current = SeefoodAI.getInstance()
     try:
         
         print 'Enter image path: '
-        # TODO: Verify and validate file path
         current.submitImg(getFilePath())
            
     except IOError as e:
@@ -32,7 +21,7 @@ def main():
 def getFilePath():
     image_path = raw_input()
     image_path = image_path.strip()
-    # Validaiton & Verification required. 
+    # TODO: Validaiton & Verification required. 
     return image_path
    
 if __name__ == "__main__":
