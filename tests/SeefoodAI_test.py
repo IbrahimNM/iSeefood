fd286d285c3049de3bb7e94268202b4c44067ca0
Ibrahim <almohaimeed.6@wright.edu>
2019-04-12 17:45:30 +0300

    Set test as a module, modifiy seefoodTest file to suite testing process

---
+++ tests/SeefoodAI_test.py
@@ -0,0 +1,22 @@
+'''
+------------------ SeefoodAI class unit test -------------------
+
+'''
+import pytest
+from iSeefood import SeefoodAI
+
+def test_submitImg():
+    ''' TODO: Test the image processing unit '''
+    assert False, "Image Submition Test"
+
+def test_pathValidation():
+    ''' TODO: Test the Validation unit '''
+    assert False, "Validation Test"
+
+def test_pathVerification():
+    '''  TODO: Test the Verification unit '''
+    assert False, "Verification Test"
+
+def test_scoresCalculation():
+    ''' TODO: Test the Score Calculation  unit '''
+    assert False, "Score Calculation Test"
