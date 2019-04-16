8a94e1e85b9011d9deb190f16eea8c1fc6e5a4b4
Ibrahim <almohaimeed.6@wright.edu>
2019-04-16 14:19:03 +0300

    Add new test cases for SeefoodAI

--- tests/SeefoodAI_test.py
+++ tests/SeefoodAI_test.py
@@ -5,34 +5,51 @@
 import pytest
 from iSeefood.SeefoodAI import SeefoodAI
 
+
 def test_process():
     ''' TODO: Test the image processing unit '''
     assert True, "Image Submition Test"
 
-def test_pathValidation():
-    ''' TODO: Test the Validation unit '''
-    current = SeefoodAI()
+
+def test_validatePath():
+    ''' Test the Validation unit '''
+    ''' Expected: only accepts a non empty string'''
+    current = SeefoodAI.getInstance()
+    
     ''' Pass an integar '''
-    pathState = current.pathValidation(10)
+    pathState = current.validatePath(10)
     assert pathState == False, "Parameter type Test #1"
 
     ''' Pass a string '''
-    pathState = current.pathValidation("sample/cookies.png")
+    pathState = current.validatePath("sample/cookies.png")
     assert pathState == True, "Parameter type Test #2"
 
     ''' Pass empty '''
-    pathState = current.pathValidation("")
+    pathState = current.validatePath("")
     assert pathState == True, "Parameter type Test #2"
 
     ''' Pass bool '''
-    pathState = current.pathValidation(True)
+    pathState = current.validatePath(True)
     assert pathState == False, "Parameter type Test #2"
 
 
-def test_pathVerification():
-    '''  TODO: Test the Verification unit '''
-    assert True, "Verification Test"
+def test_directoryExist():
+    ''' TODO: Test the directory existance checker unit '''
+    ''' Expected: Only existed directories are accepted '''
+    assert True, "Directory existance Test"
 
-def test_scoresCalculation():
-    ''' TODO: Test the Score Calculation  unit '''
-    assert True, "Score Calculation Test"
+
+def test_CheckFileExtension():
+    ''' TODO: Test file extension checker '''
+    ''' Expected: Only .png .jpg files are accepted '''
+    assert True, "File extension Test"
+
+
+def test_setScore():
+    ''' TODO: Test setting stat scores unit'''
+    assert True, "Set statistics score Test"
+
+
+def test_getScore():
+    ''' TODO: Test getting the score unit '''
+    assert True, "Get latest stat. score Test "
