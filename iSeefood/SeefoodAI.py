bb5141dfdd01ad03eba253612de80821aba9b22b
Ibrahim <almohaimeed.6@wright.edu>
2019-04-25 13:54:28 +0300

    Imrove readability of the code, fix issues and reduce # of conditional statments.

--- iSeefood/SeefoodAI.py
+++ iSeefood/SeefoodAI.py
@@ -21,17 +21,12 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 import os
-import logging
-
 
 
 class SeefoodAI(object):
     # Single private instance
     __instance = None
-    # Initilize global variables.
-    
 
-    # Initilize an AI object to be running
     def __init__(self):
         ''' Virtually private constructor '''
         if SeefoodAI.__instance != None:
@@ -53,86 +48,67 @@ class SeefoodAI(object):
     def __setup(self):
         ''' Setting-up the SeefoodAI instance'''
         # try initializing the AI instance attrs, catch possible errors.
-        
         try:
             self.sess = tf.Session()
-            print '1'
             cwd = os.getcwd()
-            print cwd
             saver = tf.train.import_meta_graph(
                 cwd+'/iSeefood/saved_model/model_epoch5.ckpt.meta')
-            saver.restore(self.sess, tf.train.latest_checkpoint(cwd+'/iSeefood/saved_model/'))
-            print '2'
+            saver.restore(self.sess, tf.train.latest_checkpoint(
+                cwd+'/iSeefood/saved_model/'))
             graph = tf.get_default_graph()
-            print '2'
             self.x_input = graph.get_tensor_by_name('Input_xn/Placeholder:0')
-            print '3'
             self.keep_prob = graph.get_tensor_by_name('Placeholder:0')
-            print '4'
             self.class_scores = graph.get_tensor_by_name("fc8/fc8:0")
         except IOError as e:
             print "--- Error: Trained model files cannot be found. Please check README file for info. ---"
 
-        # Instance has been configured 
+        # Instance has been configured
 
     def process(self, image_path):
         ''' Process valid, existed file'''
-
-        if not self.validatePath(image_path):  # Validate given path.
-            return False
-
-        # Open passed image, then convert it to RGB
-        image = Image.open(image_path).convert('RGB')
-        # Resize image to 227x227
-        image = image.resize((227, 227), Image.BILINEAR)
-        # Create a tensor
-        img_tensor = [np.asarray(image, dtype=np.float32)]
-        print '+ Looking for food in ' + image_path + ' ...... '
-        
-        # FIXME: Reduce number of conditional statments.
-        if self.class_scores is not None:
+        if self.validatePath(image_path) and self.class_scores is not None:  # Validate given path.
+            # Open passed image, then convert it to RGB
+            image = Image.open(image_path).convert('RGB')
+            # Resize image to 227x227
+            image = image.resize((227, 227), Image.BILINEAR)
+            # Create a tensor
+            img_tensor = [np.asarray(image, dtype=np.float32)]
+            print '+ Looking for food in ' + image_path + ' ...... '
             # Run the image in the model.
-            stat = self.sess.run(self.class_scores, {self.x_input: img_tensor, self.keep_prob: 1.})
+            stat = self.sess.run(self.class_scores, {
+                self.x_input: img_tensor, self.keep_prob: 1.})
             # Update score variable
             # BUG: Scores shall reset to == 0, whenever a new image is process.
             self.setScores(stat)
             print("[--------------** Given Image Has Been Analyzed **----------------]")
             # return true when an image is processed.
             return True
-        
-        
+
+        return False
 
     def validatePath(self, filePath):
         ''' Validate given file path '''
-        if isinstance(filePath, basestring):  # Verify that instance is a string type & !empty.
-            if self.checkFileExtension(filePath) and self.directoryExist(filePath):  # Verify path existance
-                print 'Good'
-                return True # check if given path ends with .png || .jpg
-        
-        return False
+        # Verify that instance is a string type & !empty && Verify path existance && given path ends with .png || .jpg.
+        return isinstance(filePath, basestring) and self.checkFileExtension(filePath) and self.directoryExist(filePath)
 
     def directoryExist(self, filePath):
         ''' Verify the existance of the given path '''
-        if os.path.exists(filePath):
-            return True  # return true when file/directory exist
-        return False # retunr false otherwise 
+        return os.path.exists(filePath)  # retunr false otherwise
 
     def checkFileExtension(self, filePath):
         ''' Verify that the given path points to an image file (png, jpg) '''
         if filePath.endswith('.png') or filePath.endswith('.jpg'):
-            return True # return true if file is valide
-        return False # return false otherwise
+            return True  # return true if file is valide
+        return False  # return false otherwise
 
     def setScores(self, stat):
         ''' Allow the AI to set the score variable '''
-        # BUG: scores accepts all data types!
+        # Set Score is not a public def., therefore no need to check data type.
         self.scores = stat
 
     def getScores(self):
         ''' Return last analyzed image stat. '''
-        
         return self.scores
-        
 
     def getResult(self, scores):
         ''' TODO: Optimaze and generate a final score'''
@@ -141,5 +117,5 @@ class SeefoodAI(object):
         # if np.argmax = 1; then the second class_score was higher, e.g., the model does not see food.
         if np.argmax(scores) == 1:
             return "+ Result:  Oops! No food here... :("
-        else:
-            return "+ Result: YAY! I see food! :)"
+
+        return "+ Result: YAY! I see food! :)"
