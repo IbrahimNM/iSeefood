7eab2c32e55f9da23fc70fa141a8780bca53e6fd
Ibrahim <almohaimeed.6@wright.edu>
2019-04-25 14:08:53 +0300

    Add more test-cases for validation units

--- tests/SeefoodAI_test.py
+++ tests/SeefoodAI_test.py
@@ -78,13 +78,11 @@ def test_validatePath4(shared_instance):
     pathState = shared_instance.validatePath(True)
     assert pathState is False, "Parameter type Test #2"
 
-
-''' Test the helper units '''
-''' Expected: only accepts a non empty string'''
+''' Test the directory existance checker unit '''
+''' Expected: Only existed directories are accepted '''
 
 def test_validateExistence(shared_instance):
-    ''' TODO: Test the directory existance checker unit '''
-    ''' Expected: Only existed directories are accepted '''
+    
     # get current working directory
     cwd = os.getcwd()
     # Valid file path
@@ -92,24 +90,34 @@ def test_validateExistence(shared_instance):
         cwd+"/iSeefood/samples/cookies.png")
     assert result is True, cwd + "/iSeefodd/samples/cookies.png directory exists"
 
+
+def test_validateExistence1(shared_instance):
     # Invalid file path
     result = shared_instance.validateExistence("samles/cookies.png")
     assert result is False, "/home/ibrahim/Desktop/iSeefood/iSeefood/samples/cookies.png directory exists"
 
+''' Test file extension checker '''
+''' Expected: Only .png .jpg files are accepted '''
 
 def test_validateExtension(shared_instance):
-    ''' Test file extension checker '''
-    ''' Expected: Only .png .jpg files are accepted '''
-
     # Valid file ext. PNG
     result = shared_instance.validateExtension("image.png")
     assert result is True, "PNG file extention test"
+
+
+def test_validateExtension1(shared_instance):
     # Valid file ext. JPG
     result = shared_instance.validateExtension("image.jpg")
     assert result is True, "JPG file extention test"
+
+
+def test_validateExtension2(shared_instance):
     # Invalid file ext. NO EXT.
     result = shared_instance.validateExtension("image")
     assert result is False, "None file extention test"
+
+
+def test_validateExtension3(shared_instance):
     # Invalid file ext. Empty
     result = shared_instance.validateExtension("")
     assert result is False, "Empty file test"
@@ -139,4 +147,5 @@ def test_getScores2(shared_instance):
     # Get score.
     result = shared_instance.getScores()
     # Check that returned value is of type nm.ndarray.
-    assert isinstance(result, numpy.ndarray), "Get score returned valid data type."
+    assert isinstance(
+        result, numpy.ndarray), "Get score returned valid data type."
