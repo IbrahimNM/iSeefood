03413a70914274943dc44b1bef2cd61882030d6f
Ibrahim <almohaimeed.6@wright.edu>
2019-04-26 12:46:01 -0400

    Fix few comments, and typos

--- iSeefood/SeefoodAI.py
+++ iSeefood/SeefoodAI.py
@@ -60,7 +60,6 @@ class SeefoodAI(object):
             stat = self.sess.run(self.class_scores, {
                 self.x_input: img_tensor, self.keep_prob: 1.})
             # Update score variable
-            # Scores shall reset to == 0, whenever a new image is process.
             self.__setScores(stat)
             print("[--------------** Given Image Has Been Analyzed **----------------]")
             # return true when an image is processed.
@@ -83,10 +82,10 @@ class SeefoodAI(object):
             return True  # return true if file is valide
         return False  # return false otherwise
 
-    def __setScores(self, stat):
+    def __setScores(self, statistics):
         ''' Allow the AI to set the score variable '''
         # Set Score is not a public def., therefore no need to check data type.
-        self.scores = stat
+        self.scores = statistics
 
     def getScores(self):
         ''' Return last analyzed image stat. '''
